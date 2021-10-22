8eaa201fc9b578c58b2f20d480e64076dae02c5e
Daryl Gaw <darylgaw97@gmail.com>
2021-10-22 11:07:15 +0800

    5. prototype

--- get_next_line.c
+++ get_next_line.c
@@ -12,42 +12,36 @@
 
 #include "get_next_line.h"
 
-static int  gnl_find_end(const char *cache);
-static int  gnl_load_cache(char **cache, int fd);
-static char *gnl_make_line(char *cache);
-static void gnl_update_cache(char **caache, char *line);
+static int  gnl_find_end(char *cache);
+static int  gnl_load_cache(char **ptr_cache, int fd);
+static char *gnl_make_line(char **ptr_cache);
+static void gnl_update_cache(char **ptr_cache, char *line);
 
-char *get_next_line(int fd)
+char    *get_next_line(int fd)
 {
     static char *cache;
-    char *line;
+    char        *line;
 
-    if (cache == NULL)
-        cache = ft_strdup("");
+    if (fd < 0)
+        return (NULL);
     while (!gnl_find_end(cache))
     {
-        if (gnl_load_cache(&cache, fd) == 0)
+        if (!gnl_load_cache(&cache, fd))
         {
-            if (*cache)
-            {
-                line = ft_strdup(cache);
-                free(cache);
-                return (line);
-            }
-            else
-            {
-                free(cache);
-                return (NULL);
-            }
+            line = gnl_make_line(&cache);
+            gnl_update_cache(&cache, line);
+            return (line);
         }
     }
-    line = gnl_make_line(cache);
+    line = gnl_make_line(&cache);
     gnl_update_cache(&cache, line);
     return (line);
 }
 
-static int gnl_find_end(const char *cache)
+static int  gnl_find_end(char *cache)
 {
+    if (cache == NULL)
+        return (0);
     while (*cache)
     {
         if (*cache++ == '\n')
@@ -56,54 +50,70 @@ static int gnl_find_end(const char *cache)
     return (0);
 }
 
-static int gnl_load_cache(char **cache, int fd)
+static int  gnl_load_cache(char **ptr_cache, int fd)
 {
-    char *buffer;
-    char *temp;
-    int i;
+    char    *buffer;
+    char    *temp;
+    size_t  i;
 
-    buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    buffer = malloc(sizeof(char) * (BUFFER_SIZE) + 1);
     if (buffer == NULL)
         return (0);
+    if (*ptr_cache == NULL)
+        *ptr_cache = ft_strdup("");
     i = read(fd, buffer, BUFFER_SIZE);
     if (i == 0)
         return (0);
     buffer[i] = '\0';
-    temp = *cache;
-    *cache = ft_strjoin(*cache, buffer);
+    temp = *ptr_cache;
+    *ptr_cache = ft_strjoin(*ptr_cache, buffer);
     free(temp);
+    free(buffer);
     return (1);
 }
 
-static char *gnl_make_line(char *cache)
+static char *gnl_make_line(char **ptr_cache)
 {
-    char *line;
-    size_t i;
+    char    *line;
+    size_t  i;
 
-    i = 0;
-    while (cache[i] != '\n')
-        i++;
-    line = malloc(sizeof(char) * (i + 2));
-    i = 0;
-    while (cache[i] != '\n')
+    if (ptr_cache == NULL)
+        return (NULL);
+    if (*(*ptr_cache) == '\0')
     {
-        line[i] = cache[i];
+        free(*ptr_cache);
+        return (NULL);
+    }
+    i = 0;
+    while ((*ptr_cache)[i] != '\n' && (*ptr_cache)[i] != '\0')
+        i++;
+    if ((*ptr_cache)[i] == '\n')
+        line = malloc(sizeof(char) * i + 2);
+    else if ((*ptr_cache)[i] == '\0')
+        line = malloc(sizeof(char) * i + 1);
+    i = 0;
+    while ((*ptr_cache)[i] != '\n' && (*ptr_cache)[i] != '\0')
+    {
+        line[i] = (*ptr_cache)[i];
         i++;
     }
-    line[i] = '\n';
-    line[++i] = '\0';
+    if ((*ptr_cache)[i] == '\n')
+        line[i++] = '\n';
+    line[i] = '\0';
     return (line);
 }
 
-static void gnl_update_cache(char **cache, char *line)
+static void gnl_update_cache(char **ptr_cache, char *line)
 {
     char *temp;
     size_t cache_len;
     size_t line_len;
 
-    temp = *cache;
-    cache_len = ft_strlen(*cache);
+    if (*ptr_cache == NULL || line == NULL)
+        return ;
+    temp = *ptr_cache;
+    cache_len = ft_strlen(*ptr_cache);
     line_len = ft_strlen(line);
-    *cache = ft_substr(*cache, line_len, cache_len - line_len);
-    free(temp);
+    *ptr_cache = ft_substr(*ptr_cache, line_len, cache_len - line_len);
+    free (temp);
 }
