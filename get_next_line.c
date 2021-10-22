fb4a5c558199ace9ac1f12a6bb98424c675a3017
darylgaw <darylgaw97@gmail.com>
2021-10-22 04:21:24 +0000

    4.

--- get_next_line.c
+++ get_next_line.c
@@ -98,7 +98,7 @@ static char	*gnl_make_line(char **ptr_cache)
 	while (*ptr_cache[i] != '\n' && *ptr_cache[i] != '\0')
 	{
 		line[i] = *ptr_cache[i];
-		i++
+		i++;
 	}
 	if (*ptr_cache[i] == '\n')
 		line[i++] = '\n';
